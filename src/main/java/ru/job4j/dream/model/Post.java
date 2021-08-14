12ce41305632968da1513e0459001690c1083313
Tatyana <anatoliy.gromov.service@gmail.com>
2021-08-14 16:27:17 +0300

    2. Scriplet. Динамическая таблица. [#276520]

---
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -0,0 +1,55 @@
+package ru.job4j.dream.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+
+    private final int id;
+    private final String name;
+    private String description = "";
+    private final Timestamp created = Timestamp.valueOf(LocalDateTime.now());
+
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
