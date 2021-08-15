f2561c889a933017f2f74d46364b2ea79fce07e0
Tatyana <anatoliy.gromov.service@gmail.com>
2021-08-15 13:06:00 +0300

    replace valueOf to parseInt

--- src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -16,7 +16,7 @@ public class CandidateServlet extends HttpServlet {
                           HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().save(new Candidate(
-                Integer.valueOf(req.getParameter("id")), req.getParameter("name")));
+                Integer.parseInt(req.getParameter("id")), req.getParameter("name")));
         resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
     }
 }
